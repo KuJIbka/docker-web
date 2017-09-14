9769f2a68de6d1c9eecdf53e63555362f0d3592a
brin <KuJIbka.polosa@gmail.com>
2017-09-14 18:50:55 +0300

    Nwe try of add libsodium and imagic

--- www/hello.dev/index.php
+++ www/hello.dev/index.php
@@ -1,4 +1,6 @@
 <?php
+phpinfo();
+
 ini_set('error_reporting', E_ALL);
 ini_set("display_startup_errors", "1");
 ini_set('display_errors', 1);
@@ -37,4 +39,4 @@ $db->exec("
 
 $st = $db->query('SELECT * FROM `test_table`');
 var_dump($st->fetchAll(\PDO::FETCH_ASSOC));
-phpinfo();
+
