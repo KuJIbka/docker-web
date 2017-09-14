2f8b8b88c63c20cc33e0b4a2db4f9e55b4811457
brin <KuJIbka.polosa@gmail.com>
2017-09-14 21:09:52 +0300

    Add default schema

--- images/mysql/database/schema.sql
+++ images/mysql/database/schema.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE `test` /*!40100 COLLATE 'utf8_general_ci' */;
+USE `test`;
 CREATE TABLE `test_table` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`some_field` VARCHAR(50) NOT NULL DEFAULT '',
