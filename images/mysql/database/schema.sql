fd643cd259bd4a025c9e6415f8e81097fa082afd
brin <KuJIbka.polosa@gmail.com>
2017-09-28 15:42:49 +0300

    Fix test schema

--- images/mysql/database/schema.sql
+++ images/mysql/database/schema.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE `simple_php` /*!40100 COLLATE 'utf8_general_ci' */;
-USE `test`;
+USE `simple_php`;
 CREATE TABLE `test_table` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`some_field` VARCHAR(50) NOT NULL DEFAULT '',
